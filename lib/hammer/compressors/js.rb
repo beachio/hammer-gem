af37103ecce69603c65154130a9e9b629b2e5247
Elliott Kember <elliott.kember@gmail.com>
2013-03-03 11:59:43 +1300

    Compressor and a

--- lib/hammer/compressors/js.rb
+++ lib/hammer/compressors/js.rb
@@ -1,7 +1,7 @@
 class Hammer
   class JSCompressor < Compressor
     def parse
-      Uglifier.compile(@text)
+      Uglifier.compile(@text)+"\n;"
     end
   end
   register_compressor JSCompressor, 'js'
