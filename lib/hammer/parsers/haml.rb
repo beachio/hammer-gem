7d7c40f6b7806c14f8c15eb4086aa5f3f43a0d5e
Elliott Kember <elliott.kember@gmail.com>
2013-05-07 19:30:19 +0100

    HAML includes, cache not so obvious

--- lib/hammer/parsers/haml.rb
+++ lib/hammer/parsers/haml.rb
@@ -24,11 +24,56 @@ class Hammer
       parse
     end
     
+    def includes
+      
+      while text.match /[\s-]*\/ @include (.*)/
+        
+        lines = []
+        replace(/[\s-]*\/ @include (.*)/) do |line, line_number|
+          
+          tags = line.gsub("/ @include ", "").strip.split(" ")
+          
+          tags.map do |tag|
+            
+            if (tag.start_with? "$")
+              variable_value = variables[tag[1..-1]]
+              
+              if !variable_value
+                raise "Includes: Can't include <b>#{h tag}</b> because <b>#{h tag}</b> isn't set."
+              end
+              
+              tag = variable_value
+            end
+            
+            file = find_file(tag, 'html')
+            
+            if file
+              if file.extension == "haml"
+                
+                indentation = line.split("/")[0].length
+                char = line.split("/")[0].split("")[0]
+                text = file.raw_text
+                if char
+                  text = text.split("\n").map {|line|  (char * indentation) + line }.join("\n")
+                end
+                text
+              else
+                "<!-- @include #{tag} -->"
+              end
+            else
+              raise "Includes: File <b>#{h tag}</b> couldn't be found."
+            end
+          end.compact.join("\n")
+        end
+      end
+    end
+    
     def to_haml
       @raw_text
     end
     
     def parse
+      includes()
       convert(text)
     rescue Haml::SyntaxError => e
       raise Hammer::Error.new(e.message, e.line)
