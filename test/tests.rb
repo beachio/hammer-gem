7acdb17062c123122987199c1a8e4272356966e4
Larry Marburger <larry@marburger.cc>
2013-08-03 15:57:56 -0400

    Fix load path in tests

--- test/tests.rb
+++ test/tests.rb
@@ -1,23 +1,18 @@
 #!/usr/bin/ruby
 
-require "rubygems"
-
-dir = File.join(File.dirname(__FILE__), "vendor/gems/*")
-Dir.glob(dir).each do |dir|
-  d = File.directory?(lib = "#{dir}/lib") ? lib : dir
-  $LOAD_PATH << d
-end
+require File.join(File.dirname(__FILE__), '..', 'lib', 'hammer', 'hammer')
 
 require "test/unit"
-require "mocha/setup"
+
+# mocha uses Gem. This might change in a later version.
+require "rubygems"
+require "mocha"
+
 require "shoulda-context"
 
 include Test::Unit
 
-require File.expand_path(File.join(File.dirname(__FILE__), "../lib/hammer/include"))
-
 tests = Dir.glob(File.join(File.dirname(__FILE__), "*.rb")) - [__FILE__]
-
 tests.each { |file|
   require File.expand_path(file)
 }
