e3d01505c77992506f87221a51d9b5d011f24be1
Larry Marburger <larry@marburger.cc>
2013-08-04 07:46:37 -0400

    Refactor integration spec
    Move spec body into a method to more easily remove tmpdirs on completion.

--- integration.rb
+++ integration.rb
@@ -9,30 +9,23 @@ end
 
 require "tmpdir"
 
-temp_directory    = Dir.mktmpdir('hammer integration')
-input_directory   = File.join('test', 'integration', 'case1')
-output_directory  = File.join(temp_directory, "Build")
-cache_directory   = File.join(temp_directory, "cache")
-
-output = nil
-error = nil
-status = nil
-
 # Here's a thing. We're not checking wait_thread's exit status, 
 # because in Ruby 1.8.7 you don't get it. Sucks to be us!
-# Larry told me ot use open3 instead of backticks.
+# Larry told me to use open3 instead of backticks.
 
 require "open3"
 include Open3
 
-[false, true].each do |optimization|
-  FileUtils.rm_rf output_directory
-  FileUtils.rm_rf cache_directory
-  
-  puts
-  puts "Testing #{optimization ? 'optimized' : 'standard'} integration"
-  
-  optimized = optimization ? "PRODUCTION" : ""
+def run_integration_test(optimized)
+  input_directory  = File.join('test', 'integration', 'case1')
+  output_directory = Dir.mktmpdir('Build')
+  cache_directory  = Dir.mktmpdir('cache')
+
+  output = nil
+  error = nil
+  status = nil
+
+  puts "Testing #{optimized ? 'optimized' : 'standard'} integration"
   
   Open3.popen3('/usr/bin/ruby', 'hammer_time.rb',
                cache_directory, input_directory, 
@@ -58,6 +51,13 @@ include Open3
   assert "our output doesn't contain errors" do
     !output.include? "build-error"
   end
-  
+
   puts
+ensure
+  FileUtils.remove_entry output_directory
+  FileUtils.remove_entry cache_directory
+end
+
+[false, true].each do |optimized|
+  run_integration_test optimized
 end
