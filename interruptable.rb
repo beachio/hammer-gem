ad4266866602065c1ac0f4dbdf92f31ba4bb68be
Elliott Kember <elliott.kember@gmail.com>
2013-08-05 15:31:43 +0100

    Interruptable stub

--- interruptable.rb
+++ interruptable.rb
@@ -1,27 +1,4 @@
-# encoding: utf-8
-$LANG = "UTF-8"
-
-require File.join(File.dirname(__FILE__), "lib/hammer/hammer")
-require 'hammer/build'
-
-# TODO: Remove this file. We're moving to hammer_time.rb with PRELOAD.
-
-# Pause to prevent the UI from returning too quickly and wreaking havoc with
-# FSEvents.
-def not_too_fast(start, minimum_duration = 0.5)
-  duration = Time.now - start
-  sleep minimum_duration - duration if duration < minimum_duration
-end
-
-build = Hammer::Build.new(:cache_directory   => ARGV[0],
-                          :project_directory => ARGV[1],
-                          :output_directory  => ARGV[2],
-                          :optimized   => ARGV.include?('PRODUCTION'))
-
-start = Time.now
-build.stop_hammer_time! do |project, app_template|
-  not_too_fast(start)
-
-  puts app_template
-  exit app_template.success? ? 0 : 1
-end
+args = ARGV
+args << 'PRELOAD'
+hammer_time = File.expand_path(File.join(File.dirname(__FILE__), 'hammer_time.rb'))
+exec 'ruby', hammer_time, *args
