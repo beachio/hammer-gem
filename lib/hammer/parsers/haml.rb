2fc9213ef8b7b304676d6c87baba3c7e1c20276e
Elliott Kember <elliott.kember@gmail.com>
2013-09-05 16:55:49 +0100

    Time to refactor this garbage

--- lib/hammer/parsers/haml.rb
+++ lib/hammer/parsers/haml.rb
@@ -41,11 +41,10 @@ class Hammer
           
           number_of_indents_in_this_line = line[/\A[ |\t]*/].size
           next_line = lines[line_number+1]
-          puts "Line after include:"
-          puts next_line
+
           number_of_indents_in_the_next_line = next_line[/\A[ |\t]*/].size 
           indented_after_this_line = number_of_indents_in_this_line < number_of_indents_in_the_next_line
-          
+
           tag = line.gsub("/ @include ", "").strip.split(" ")[0]
           file = find_file(tag, 'html')
           
@@ -57,22 +56,32 @@ class Hammer
             lines[line_number] = file.raw_text.split("\n").map {|line| letters_to_indent_by + line }
             
             if indented_after_this_line
+
               indents_at_this_line = line[/\A[ |\t]*/].size
-              included_file_last_line_indentation = file.raw_text.split("\n")[-1][/\A[ |\t]*/].size
+
+              included_file_last_line_indentation = file.raw_text.lines.map(&:chomp)[-1][/\A[ |\t]*/].size
               
+              # Skip forward until we find a line with the same number of indents.
               i = line_number + 1
               
               while i < lines.length
-                if lines[i][/\A[ |\t]*/].size > indents_at_this_line
-                  
-                  letters_to_indent_by = line.split("")[0] * line[/\A[ |\t]*/].size
-                  
+
+                indents_at_the_future_line = lines[i][/\A[ |\t]*/].size
+
+                if indents_at_the_future_line > indents_at_this_line
+                  indent_character = lines[i].split("").first
+                  letters_to_indent_by = indent_character * (included_file_last_line_indentation)
+
                   lines[i] = letters_to_indent_by + lines[i]
+                else
+                  break
                 end
-                i+=1
+
+                i += 1
               end
             else
-              
+              # We don't need to do anything here. 
+              # This include gets taken care of.
             end
             lines = lines.flatten
           else
@@ -85,6 +94,10 @@ class Hammer
       end
       
       @text = lines.join("\n")
+    rescue => e
+      puts lines.join("\n")
+puts e.message
+puts e.backtrace
     end
      
     def to_haml
