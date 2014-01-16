c3ff3b55aa63866a8068028e3b318f61b12496e2
Elliott Kember <elliott.kember@gmail.com>
2014-01-16 16:57:02 +0000

    Fix for reload tag change in functional test

---
+++ test/hammer/integration_test.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/test_helper'
+
+class HammerGemIntegrationTest < Test::Unit::TestCase
+  def test_gem_integrates
+    # assert false
+  end
+end
