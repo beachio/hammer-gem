c25787381f2eca38b409c9c1756a9a95676bc12f
Elliott Kember <elliott.kember@gmail.com>
2014-01-17 12:01:41 +0000

    Application template removing cruft

--- lib/hammer/templates/application/application_template_line_template.rb
+++ lib/hammer/templates/application/application_template_line_template.rb
@@ -5,12 +5,11 @@ module Hammer
 
     def initialize(options)
       @hammer_file = options[:hammer_file]
-      @filename = @hammer_file.filename
     end
 
     def to_s
       template = ERB.new File.new(File.join(File.dirname(__FILE__), "file.html.erb")).read, nil, "%"
-      "\n"+template.result(binding)
+      "\n#{template.result(binding)}"
     end
 
     def span_class
@@ -19,7 +18,7 @@ module Hammer
       classes << "optimized" if @hammer_file.is_a_compiled_file
       classes << "error" if @error
       classes << "include" if @include
-      classes << "include" if @hammer_file.filename.start_with? "_"
+      classes << "include" if filename.start_with? "_"
       classes << "cached" if from_cache
       classes << @extension
       classes << 'image' if ['png', 'gif', 'svg', 'jpg', 'gif'].include? @extension
@@ -33,37 +32,31 @@ module Hammer
     end
 
     extend Forwardable
-    def_delegators :@hammer_file, :path, :from_cache, :output_filename, :messages, :output_path, :ignored, :include?, :extension, :error
+    def_delegators :@hammer_file, :path, :from_cache, :output_filename, :filename,
+                   :messages, :output_path, :ignored, :include?, :extension, :error
 
     def reveal
-      !@filename.start_with?(".")
+      !filename.start_with?(".")
     end
 
     def show_open_in_browser
-      @filename.end_with?(".html") && output_path
+      filename.end_with?(".html") && output_path
     end
 
     def show_edit_link
-      ['.html', ".css", ".js"].include?(File.extname(@filename)) || @filename.start_with?(".")
+      ['.html', ".css", ".js"].include?(File.extname(filename)) || filename.start_with?(".")
     end
+
     def html
       @hammer_file.extension == "html"
     end
 
     def compiled
-      @hammer_file.is_a_compiled_file || html
+      @hammer_file.is_a_compiled_file or html
     end
 
     def error_filename
-      return error.error_file.filename if error
+      error.error_file.filename if error
     end
-
-    # <% # combiled file sources %>
-    # <% # sources = @file.source_files.map { |hammer_file| "<a href='#{@file.output_path}' title='#{hammer_file.full_path}'>#{File.basename(hammer_file.filename)}</a>" }a %>
-
-    # def include?
-    #   @filename.split()[0] == "_"
-    # end
-
   end
 end
