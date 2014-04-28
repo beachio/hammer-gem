cfe32d3a0ca33f67db04a7b40b5b10b6977152a0
Elliott Kember <elliott.kember@gmail.com>
2014-04-28 12:15:54 +0100

    Moved tests around

--- test/hammer/test_helper.rb
+++ test/hammer/test_helper.rb
@@ -1,1 +1,43 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'rake'
+
+begin
+  require "simplecov"
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  SimpleCov.start do
+    add_filter 'test'
+    add_group 'Hammer', '/lib/hammer'
+    add_group 'Parsers', '/lib/hammer/parsers/*'
+    add_group 'Templates', '/lib/hammer/templates'
+    add_group 'Utilities', '/lib/hammer/templates'
+    ENV['COVERAGE'] = 'true'
+    Rake::Task["test"].execute
+  end
+rescue LoadError
+  # not installed
+end
+
+require 'fileutils'
+
+lib_dir = File.dirname(__FILE__) + '/../lib'
+$:.unshift lib_dir unless $:.include?(lib_dir)
+require 'hammer/hammer'
+
+require 'test/unit'
+require 'sass'
+require 'mathn' if ENV['MATHN'] == 'true'
+require 'tmpdir'
+require 'mocha/setup'
+require 'shoulda'
+
+Sass::RAILS_LOADED = true unless defined?(Sass::RAILS_LOADED)
+Encoding.default_external = 'UTF-8' if defined?(Encoding)
+
+def create_file(filename, contents, directory=nil)
+  directory ||= Dir.mktmpdir
+  path = File.join(directory, filename)
+  FileUtils.mkdir_p(File.dirname(path))
+  File.open(path, 'w') do |file|
+    file.print contents
+  end
+  path
+end
