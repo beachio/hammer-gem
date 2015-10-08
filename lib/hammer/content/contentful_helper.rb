f484bfe7df92eab68fd750be9c120306c766783e
Alexander <alex.hh@me.com>
2015-10-08 23:23:54 +0300

    implemented contentful content types mapping and multispaces

--- lib/hammer/content/contentful_helper.rb
+++ lib/hammer/content/contentful_helper.rb
@@ -2,24 +2,49 @@ require 'contentful'
 require 'ostruct'
 module Hammer
   class ContentfulHelper
-    def initialize(config)
+    def initialize(config, space_name = 'default')
       return unless config
+      @config, @space = config, config['spaces'][space_name]
       @client = Contentful::Client.new(
         access_token: config['apiKey'],
-        space: config['spaces']['default']['id']
+        space: @space['id']
       )
     end
 
-    def entries
-      @entries_raw ||= @client.entries
-      parse_entries(@entries_raw)
+    def entries(query = {})
+      @entries_raw ||= {}
+      @entries_raw[query.hash] ||= @client.entries(query.merge(include: 2))
+      parse_entries(@entries_raw[query.hash])
+    end
+
+    def entries_by_content_type(name)
+      entries(content_type: content_type_id(name))
     end
 
     private
 
+    # we catch requests to different content types and other spaces by this hack
+    # like:
+    # contentful.articles -> method_missing...
+    # contentful.my_awesome_space.posts -> method_missing...
+    def method_missing(method_name, *arguments, &block)
+      key = method_name.to_s
+      # lets check what user needs.
+      # do we need to respond with different space?
+      if @config['spaces'].has_key? key
+        # return new instance of helper with different space
+        @helpers ||= {}
+        @helpers[key] ||= Hammer::ContentfulHelper.new(@config, key)
+      # do we need a return entries of specific content type?
+      elsif @space['contentTypes'].has_key? key
+        entries_by_content_type(@space['contentTypes'][key])
+      else
+        super
+      end
+    end
+
     def parse_entries(entries)
-      return @entries_parsed if @entries_parsed
-      @entries_parsed = entries.map do |entry|
+      entries.map do |entry|
         parse_entry(entry)
       end.compact
     end
@@ -38,11 +63,19 @@ module Hammer
         then content.map { |x| parse_content(x) }
       when /Contentful::Asset/
         then content.image_url
+      when /Contentful::Entry/
+        then parse_entry(content)
       when /String|Fixnum|Float|Bignum|BigDecimal/
         then content
-      else
-        nil
       end
+    end
+
+    def content_type_id(name)
+      return @content_type_ids[name] if @content_type_ids
+      @content_type_ids = @client.content_types.map do |content|
+        [content.properties[:name], content.id]
+      end.to_h
+      @content_type_ids[name]
     end
   end
 end
