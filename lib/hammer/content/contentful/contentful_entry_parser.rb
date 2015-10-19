5841745312cd2c3f9ef25baff274c41f80cd3fea
Alexander <alex.hh@me.com>
2015-10-19 14:36:45 +0300

    limit parsing level (stack too deep error)

--- lib/hammer/content/contentful/contentful_entry_parser.rb
+++ lib/hammer/content/contentful/contentful_entry_parser.rb
@@ -3,10 +3,17 @@ require 'ostruct'
 
 module Hammer
   class ContentfulEntryParser
-    def parse(entry)
+    attr_accessor :max_level
+
+    def initialize(max_level = 3)
+      self.max_level = max_level
+    end
+
+    def parse(entry, level = 0)
+      return OpenStruct.new if level >= max_level
       os = OpenStruct.new
       entry.fields.each do |field, content|
-        os[field] = parse_content(content)
+        os[field] = parse_content(content, level + 1)
       end
       # os['type'] = content_type_name(entry.sys[:contentType].sys[:id])
       os
@@ -14,16 +21,17 @@ module Hammer
 
     private
 
-    def parse_content(content)
+    def parse_content(content, level = 0)
       case content.class.to_s
       when /Array/
-        then content.map { |x| parse_content(x) }.compact
+        then content.map { |x| parse_content(x, level + 1) }.compact
       when /Contentful::Asset/
         then content.image_url
       when /Contentful::Entry/
-        then parse(content)
-      when /Contentful::Link/
-        then parse_content(content.resolve) rescue nil
+        then parse(content, level + 1)
+      when /Contentful::Link/ then
+        return content if level >= max_level
+        parse_content(content.resolve, level + 1) rescue nil
       else content
       end
     end
